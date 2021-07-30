1b9fae6d07c6c868415f9684dbec15ea9408f11c
Thanapat <thanapat.war@ku.th>
2021-07-30 18:37:11 +0700

    first

---
+++ src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>Welcome</h1>
+    <div>
+        <label >Username</label>
+        <input type="text">
+    </div>
+    <div>
+        <label >Password</label>
+        <input type="password">
+    </div>
+    <div>
+        <button @click="next">Login</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    next(){
+      this.$router.push('/homePage')
+    }
+
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <home></home>
+  </div>
+</template>
+
+<script>
+import Home from '@/components/Home.vue'
+export default {
+    components: {
+    Home
+  }
+}
+</script>
