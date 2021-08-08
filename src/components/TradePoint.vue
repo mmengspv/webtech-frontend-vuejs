88f98290d349fcdb3b914153f62291e4832de604
Itsaraphap <Itsaraphap.s@ku.th>
2021-08-08 14:52:56 +0700

    Try to trade point for update user point in UserApi

--- src/components/TradePoint.vue
+++ src/components/TradePoint.vue
@@ -1,35 +1,41 @@
 <template>
   <div>
-    <h3>Trade</h3>
-      <span>
-        <tbody>
-          <tr v-for="(reward, index) in rewards" :key="index">
-            <div class="card" style="width: 10rem;">
-              <img class="card-img-top" :src="api_endpoint + reward.image[0].url">
-              <div class="card-body">
-                <h5 class="card-title">{{ reward.reward_name }}</h5>
-                <p class="card-text">{{ reward.detail }}</p>
-                <button>Use Point</button>
-              </div>
-            </div>
+    <navbar></navbar>
+    <h3>Trading Point</h3>
+    <div class="card-group">
+      <div class="col-sm-4" v-for="(reward, index) in rewards" :key="index">
+        <div class="card mx-auto mt-3" style="width: 35%">
+          <img
+            class="card-img-top"
+            :src="api_endpoint + reward.image[0].url"
+            height="200px"
+          />
+          <div class="card-body">
+            <h5 class="card-title">{{ reward.reward_name }}</h5>
+            <h6 class="card-subtitle">{{reward.exchange_point}} Point</h6>
+            <p class="card-text">{{ reward.detail }}</p>
+            <button>Use Point</button>
+          </div>
+        </div>
+      </div>
+    </div>
 
-          </tr>
-        </tbody>
-      </span>
+
   </div>
 </template>
 
 <script>
-
+import Navbar from '../components/Navbar.vue';
 import RewardApiStore from "@/store/RewardApi";
 export default {
-components: {
-    
-    },
-    data() {
+  components: {
+    Navbar
+  },
+  data() {
     return {
       rewards: [],
       api_endpoint: process.env.VUE_APP_STRAPI_API,
+      users:[],
     };
   },
   created() {
@@ -40,9 +46,23 @@ components: {
       await RewardApiStore.dispatch("fetchReward");
       this.rewards = RewardApiStore.getters.rewards;
     },
+    async editPoint() {
+      let payload = {
+        index: this.id,
+        point: this.point,
+      };
+      let res = await UserApi.dispatch("editPoint", payload);
+      if (res.success) {
+      } else {
+        this.$swal("Add Failed", res.message, "error");
+      }
+    }
   },
-}
+};
 </script>
 
 <style>
+.ml-1 {
+  margin-top: 15px;
+}
 </style>
