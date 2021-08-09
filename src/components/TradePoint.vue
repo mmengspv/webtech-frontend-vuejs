7eb0a8a3134f8429c5b5f37834e448e71a2ea53a
Thanapat <thanapat.war@ku.th>
2021-08-09 17:04:52 +0700

    tradepoint swal

--- src/components/TradePoint.vue
+++ src/components/TradePoint.vue
@@ -55,28 +55,40 @@ export default {
       this.users = UserApi.getters.users
     },
     async editPoint(reward) {
-      const user = await UserService.getUserById(this.user_id);
-      let payload_user = {
-        index: this.user_id,
-        point: user.point-reward.exchange_point,
-      };
-      await UserApi.dispatch("updatePoint", payload_user);
-      let payload_point = {
-        type: "use",
-        point: reward.exchange_point,
-        user: this.user_id,
-      };
-      // console.log(this.point);
-      await PointApi.dispatch("addPoint", payload_point);
-      let payload_reward = {
-        id:reward.id,
-        reward_name: reward.reward_name,
-        exchange_point: reward.exchange_point,
-        detail: reward.detail,
-        amount: reward.amount-1,
-        image: reward.image_id,
-      };
-      await RewardApiStore.dispatch("editReward", payload_reward);
+      if(reward.amount > 0){
+        const user = await UserService.getUserById(this.user_id);
+        if(reward.exchange_point < user.point){
+          let payload_user = {
+            index: this.user_id,
+            point: user.point-reward.exchange_point,
+          };
+          await UserApi.dispatch("updatePoint", payload_user);
+          const today= new Date()
+          let payload_point = {
+            
+            type: "use",
+            point: reward.exchange_point,
+            date: today,
+            user: this.user_id,
+          };
+          // console.log(this.point);
+          await PointApi.dispatch("addPoint", payload_point);
+          let payload_reward = {
+            id:reward.id,
+            reward_name: reward.reward_name,
+            exchange_point: reward.exchange_point,
+            detail: reward.detail,
+            amount: reward.amount-1,
+            image: reward.image_id,
+          };
+          await RewardApiStore.dispatch("editReward", payload_reward);
+          this.$swal("Successful redemption", `You have successfully redeemed ${reward.reward_name}`, "success");
+        }else{
+          this.$swal("Rewards cannot be redeemed now", "Your points are not enough", "warning");
+        }
+      }else{
+        this.$swal("Rewards cannot be redeemed now", "Rewards out of stock", "warning");
+      }
     },
     async thisUser(){
       
